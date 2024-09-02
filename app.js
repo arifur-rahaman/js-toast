0d724a8207c65f0adb941e2cfe92184626e39d6a
unknown <JNKarif>
2024-09-02 16:57:18 +0600

    toaster funciton added

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-function showToast() {}
+let toastBox = document.getElementById("toast-box");
+
+const successFull = "Successfully Submitted";
+const errorMsg = "Error! Please fix this error";
+const invalidInput = "Invalid! Please give valid input";
+
+function showToast(massage) {
+  let toast = document.createElement("div");
+  toast.classList.add("toast");
+  toast.innerHTML = massage;
+  toastBox.appendChild(toast);
+}
