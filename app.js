5ad69f90e2fb4251d7cb6828c5283c9f9519e449
unknown <JNKarif>
2024-09-02 17:09:08 +0600

    fa icons for toaster

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 let toastBox = document.getElementById("toast-box");
 
-const successFull = "Successfully Submitted";
-const errorMsg = "Error! Please fix this error";
-const invalidInput = "Invalid! Please give valid input";
+const successFull =
+  "<i class='fa-solid fa-circle-check'></i> Successfully Submitted";
+const errorMsg =
+  "<i class='fa-solid fa-circle-xmark'></i> Error! Please fix this error";
+const invalidInput =
+  "<i class='fa-solid fa-circle-exclamation'></i>  Invalid! Please give valid input";
 
 function showToast(massage) {
   let toast = document.createElement("div");
